b84895e4f20f56bdd128be7eacc1a0f6dcfcd33c
Raghav Kapoor <raghav.kapoor.in@ad.fc.net>
2022-01-13 00:30:36 -0800

    Added a functionality of multiply

--- demo.java
+++ demo.java
@@ -1,13 +1,19 @@
 class calculator
 { 
-void add(int a,int b)
+int add(int a,int b)
 {
 return a+b;
 }
-void subtract(int a,int b)
+int subtract(int a,int b)
 {
 return a-b;
 }
+int multiply(int a, int b)
+{
+return a*b;
+}
+
+
 
 }
 public class simple
@@ -17,6 +23,7 @@ public static void main(String[] args)
 calculator c1=new calculator();
 System.out.println(c1.add(4,5));
 System.out.println(c1.subtract(6,5));
+System.out.println(c1.multiply(6,5));
 
 }
 }
